01789cbd8479994f885d8cbaec6d670697e3ddf3
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-05-14 18:34:56 +0700

    fix signup form

--- client/src/components/Signup.vue
+++ client/src/components/Signup.vue
@@ -1,27 +1,33 @@
 <template>
-  <div class="eight wide column">
+  <div class="signup-container">
 
-    <h3> Sign up</h3>
-    <form class="ui huge form" v-on:submit="onSubmitSignUpForm" >
-      <div class="field">
-        <input type="text" name="name" v-model="user_form.name" placeholder="Full Name">
-      </div>
-      <div class="field">
-        <input type="text" name="username" v-model="user_form.username" placeholder="Username">
-      </div>
-      <div class="field">
-        <input type="text" name="email" v-model="user_form.email" placeholder="Email">
-      </div>
-      <div class="field">
-        <input type="text" name="phone" v-model="user_form.phone" placeholder="Mobile Phone">
-      </div>
+    <div class="ui one column stackable center aligned page grid">
+       <div class="column twelve wide">
 
-      <div class="field">
-        <input type="password" name="last-name" v-model="user_form.password" placeholder="Password">
-      </div>
+        <h1>Sign up</h1>
+        <form class="ui huge form" v-on:submit="onSubmitSignUpForm" >
+          <div class="field">
+            <input type="text" name="name" v-model="user_form.name" placeholder="Full Name">
+          </div>
+          <div class="field">
+            <input type="text" name="username" v-model="user_form.username" placeholder="Username">
+          </div>
+          <div class="field">
+            <input type="text" name="email" v-model="user_form.email" placeholder="Email">
+          </div>
+          <div class="field">
+            <input type="text" name="phone" v-model="user_form.phone" placeholder="Mobile Phone">
+          </div>
 
-      <button id="btnSubmitSignUp" class="ui button green right floated" type="submit">Sign up</button>
-    </form>
+          <div class="field">
+            <input type="password" name="last-name" v-model="user_form.password" placeholder="Password">
+          </div>
+
+          <button id="btnSubmitSignUp" class="ui huge button green center floated" type="submit">Sign up</button>
+        </form>
+
+      </div>
+    </div>
 
   </div> <!-- column -->
 </template>
@@ -58,11 +64,11 @@
 	            console.log(response.data);
 	            // console.log(err);
 	            console.log("**** sign up error")
-	            
+
 	        } else {
 	            console.log("**** sign up successful")
 	            console.log(response);
-	            
+
 	            console.log(self);
 	            console.log(self.$router);
 	            self.$router.push('/');
@@ -81,5 +87,14 @@
 
 
 <style scoped>
-	
+.signup-container {
+  margin: auto;
+  width: 100%;
+  margin-top: 20px;
+  background: #FDD692;
+  border-radius: 10px;
+
+  padding: 10px;
+
+}
 </style>
